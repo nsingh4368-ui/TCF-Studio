88609b4525bcd0ce3652a42b50d9d7a47c3a0ad2
nsingh4368-ui <nsingh4368@gmail.com>
2026-06-12 22:55:52 +0530

    Update state.js

--- src/core/state.js
+++ src/core/state.js
@@ -1,1 +1,9 @@
-export const state={theme:'light',activeTask:1};
+export const state = {
+  activeTask: 1,
+
+  limits: {
+    1: { min: 60, max: 120 },
+    2: { min: 120, max: 150 },
+    3: { min: 180, max: 180 }
+  }
+};
